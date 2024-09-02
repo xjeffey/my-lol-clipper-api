a583f02e526d2d4496e98b67a6a23a6fe1baf862
Jeff Tran <tranjeff@live.com>
2024-09-01 22:09:32 -0400

    add controller

---
+++ MyLoLClipperAPI/MyLoLClipperAPI/Controllers/MyLoLClipperAPIController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyLoLClipperAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MyLoLClipperAPIController : ControllerBase
+    {
+    }
+}
