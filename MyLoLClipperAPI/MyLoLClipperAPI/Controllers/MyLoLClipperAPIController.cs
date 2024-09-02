e17ffb0c665104dc332eca0d4f8d042dbab4fd2e
Jeff Tran <tranjeff@live.com>
2024-09-01 22:37:23 -0400

    testing api changes

--- MyLoLClipperAPI/MyLoLClipperAPI/Controllers/MyLoLClipperAPIController.cs
+++ MyLoLClipperAPI/MyLoLClipperAPI/Controllers/MyLoLClipperAPIController.cs
@@ -7,5 +7,28 @@ namespace MyLoLClipperAPI.Controllers
     [ApiController]
     public class MyLoLClipperAPIController : ControllerBase
     {
+        private IConfiguration _configuration;
+
+        public MyLoLClipperAPIController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        [Route("GetAPIKey")]
+        public string GetAPIKey()
+        {
+            string apikey = "";
+
+            var config = new ConfigurationBuilder()
+                .AddUserSecrets<Program>()
+                .Build();
+
+            if (!string.IsNullOrWhiteSpace(config["apikey"].ToString())) {
+                apikey = config["apikey"];
+            }
+
+            return apikey;
+        }
     }
 }
--- MyLoLClipperAPI/MyLoLClipperAPI/obj/Debug/net8.0/MyLoLClipperAPI.AssemblyInfo.cs
+++ MyLoLClipperAPI/MyLoLClipperAPI/obj/Debug/net8.0/MyLoLClipperAPI.AssemblyInfo.cs
@@ -11,10 +11,11 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("94468e89-3c90-4660-afd5-012e4cdb7480")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyLoLClipperAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a583f02e526d2d4496e98b67a6a23a6fe1baf862")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyLoLClipperAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyLoLClipperAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
